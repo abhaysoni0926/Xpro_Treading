d3f7372a0d0964f3806347ab88b76359fe0979ed
abhaysoni0526 <abhaysonish90090@gmail.com>
2024-08-25 15:38:31 +0530

    first commit

---
+++ Frontend-React/src/Api/api.js
@@ -0,0 +1,19 @@
+
+import axios from 'axios';
+//const DEPLOYED = 'https://xprotread-f9c91d7e23c1.herokuapp.com/'
+
+ const LOCALHOST='http://localhost:5454'
+
+export const API_BASE_URL = LOCALHOST
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+const token = localStorage.getItem('jwt');
+
+api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+api.defaults.headers.post['Content-Type'] = 'application/json';
+
+export default api;
